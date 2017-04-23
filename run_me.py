310a676d8d8b807096358509407b7a37f3cf00cf
Van Nguyen <vann@umass.edu>
2017-04-22 23:52:45 -0400

    Update GridSearch

--- run_me.py
+++ run_me.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import cluster_class 
+# import cluster_class 
 from sklearn.cluster import KMeans
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import pairwise_distances
@@ -8,8 +8,17 @@ from sklearn.metrics import pairwise_distances
 
 
 # Load data 
-train = pd.read_csv('train.csv')
-test = pd.read_csv('test.csv')
+DATA_PATH = '/Users/VanNguyen/Desktop/COMPSCI589/Final Project/Data/'
+
+column_names = ['season','hour','holiday','workingday','weathersit',
+'feeling_temp','humidity','windspeed','duration','station','type']
+
+train = pd.read_csv(DATA_PATH + 'train.csv')
+test = pd.read_csv(DATA_PATH + 'test.csv')
+
+train.columns = column_names
+test.columns = column_names
+
 X_train = train.iloc[:, :-1]
 y_train = train.iloc[:, -1]
 X_test = test.iloc[:, :-1]
@@ -43,55 +52,65 @@ def cluster_quality(X,Z,K):
 
 # CV for KMeans model-----------------------
 
-# make station column into categorical variable
-X_train['station'] = X_train['station'].astype('category')
+def make_dummy(X):
+    # make station column into categorical variable
+    # create dummy variables for all stations
+    # column bind original variables with station indicator variables
 
-# create dummy variables for all stations
-train_dummy = pd.get_dummies(X_train['station'])
+    stations = X['station'].unique()
+    dummy = pd.get_dummies(X.loc[:, 'station'])
+    X = pd.concat([X, dummy], axis=1, ignore_index = True)
+    X.columns = np.hstack((column_names[:-1], stations))
+    X.drop('station', axis=1, inplace=True)
 
-# column bind original variables with station indicator variables
-X_train = pd.concat([X_train, train_dummy], axis=1)
+    return X
 
-# Remove station column now that we have indicators
-# This avoids having any strings in the data
-X_train = X_train.drop(['station'], axis=1)
+def KMeansCV(X, range_k, range_r):    
+    # create dictionary of parameters for the Grid Search
+    # parameters are number of clusters and number of random restarts for kmeans
+    params = {'n_clusters': range_k, 'random_state': range_r}
 
-# remove date variable to avoid working with dates
-# already have season, holiday, work day, hour indicators 
-X_train = X_train.drop(['date'], axis=1) 
-#X_train['date'] = pd.to_datetime(X_train['date'])
+    # initialize kmeans
+    km = KMeans()
 
-# convert to numpy array for input to KMeans
-X_train_array = np.array(X_train)
+    # conduct grid search over ranges of hyperparameters on training data
+    clf = GridSearchCV(km, params)
+    clf.fit(X)
 
-# follow the same steps for the test data set 
-X_test['station'] = X_test['station'].astype('category')
-test_dummy = pd.get_dummies(X_test['station'])
-X_test = pd.concat([X_test, test_dummy], axis=1)
-X_test = X_test.drop(['station'], axis=1)
-X_test = X_test.drop(['date'], axis=1) # removes date variable
-#X_test['date'] = pd.to_datetime(X_test['date'])
-X_test_array = np.array(X_test)
-
-# create dictionary of parameters for the Grid Search
-# parameters are number of clusters and number of random restarts for kmeans
-params = {'n_clusters':[1,40], 'random_state':[1,10]}
-
-# initialize kmeans
-km = KMeans()
-
-# conduct grid search over ranges of hyperparameters on training data
-clf = GridSearchCV(km, params)
-clf.fit(X_train_array)
-
-# grab optimal parameters; use as input for kmeans 
-optimal_k = clf.get_params()
-print optimal_k
-
+    # grab optimal parameters; use as input for kmeans 
+    optimal_params = clf.best_params_
+    
+    return optimal_params
 
 
 
 # CV for classification
+
+
+
+if __name__ == '__main__':
+
+    X_train_dummy = make_dummy(X_train)
+    # X_test_dummy = make_dummy(X_test)
+
+    range_k = range(3)
+    range_r = range(5)
+    best_params = KMeansCV(X = X_train_dummy, range_k = [1,2,3,4], range_r = [1,2,3,4,5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
