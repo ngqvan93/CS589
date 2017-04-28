54e2b58c8e8336f504216c780fb6f72cac6aea3e
Van Nguyen <vann@umass.edu>
2017-04-28 16:01:49 -0400

    Update run_me.py

--- cluster_class.py
+++ cluster_class.py
@@ -17,19 +17,19 @@ class cluster_class:
         self.kmeans = KMeans(n_clusters = self.K, random_state = r)
         self.clusters = {}
         self.clf = {}
+        self.proportions = {}
 
-    def classify_one_cluster(self, k, clf, *args):
+    def classify_one_cluster(self, k, clf, args):
         X = self.clusters[k][0]
         y = self.clusters[k][1]
         clf = clf(args)
         clf.fit(X, y)
         self.clf[k] = clf
 
-    def clustering(self, X, Y):
+    def clustering(self, X, y):
         '''
         Cluster data and store to dictionary
         '''
-
         # Fit a K-Means clustering model.
         self.kmeans = self.kmeans.fit(X)        
         Z = self.kmeans.labels_
@@ -41,19 +41,31 @@ class cluster_class:
 
             # Check if there is any case assigned to cluster kth.
             if len(ix[0]) > 0:
-                Y_k = Y[ix]
+                y_k = Y[ix]
                 X_k = X[ix]
                 self.clusters[k] = (X_k, y_k)   
             else:
-                self.labels[k] = random.choice(Y)
+                label = random.choice(y)
+                if label == 1:
+                    self.proportions[k] = (1, 0)
+                else:
+                    self.proportions[k] = (0, 1)
 
+
+    def fit(self, X, y, clf, args):
+        self.clustering(X, y)
+        for k in range(self.K):
+            self.classify_one_cluster(k, clf, args)
 
 
     def predict(self, X):
         '''
-        Make predictions usins a cluster classifier object
+        Make predictions using a cluster classifier object
         '''        
-        pass
+        for k in xrange(K):
+            predictions = self.clf[k].predict(X)
+            prop = float(sum(predictions))/len(predictions)
+            self.proportions[k] = (prop, 1-prop)
 
     
     def score(self,X,Y):
